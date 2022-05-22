3527c5b81dd22c5100996690d72bf80c94e26612
Andrew M <bionicmaan@gmail.com>
2022-05-22 14:40:43 +0300

    Причесан pom.xml

--- src/test/java/ru/otus/APIHelpers/tests/RestAssuredTests.java
+++ src/test/java/ru/otus/APIHelpers/tests/RestAssuredTests.java
@@ -2,6 +2,7 @@ package ru.otus.APIHelpers.tests;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,7 +22,7 @@ class RestAssuredTests {
 				.extract().response();
 
 		System.out.println(response.getBody().prettyPrint());
-		Assertions.assertEquals(200, response.statusCode());
+		Assertions.assertEquals(HttpStatus.SC_OK, response.statusCode());
 		Assertions.assertEquals("Janet", response.jsonPath().getString("data.first_name"));
 		Assertions.assertEquals("Weaver", response.jsonPath().getString("data.last_name"));
 	}
